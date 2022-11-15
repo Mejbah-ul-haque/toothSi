352d03acb8586ace48b1d3ce0943880f94c3ed5f
Mejbah <mejba.s@gmail.com>
2022-11-15 09:31:41 +0600

    First comment for ToothSi

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './component/Home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Ok</h1>
+      <button className='btn btn-secondary'>Button</button>
+      <Routes>
+        <Route path='/' element={<Home />}>
+          
+        </Route>
+      </Routes>
     </div>
   );
 }
